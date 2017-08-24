a51cf6b8014b61b0e1c29f3dfe122f4248a92ee1
Dorren Chen <dorrenchen@gmail.com>
2017-08-23 23:49:50 -0400

    replace private static methods with const

--- app/puzzles/node.js
+++ app/puzzles/node.js
@@ -8,49 +8,46 @@ class Node {
   hasLeft()  { return this.left  != null; }
   hasRight() { return this.right != null; }
 
-  static _height(node) {
-    if(node == null) { return 0; }
-
-    var left_h  = this._height(node.left);
-    var right_h = this._height(node.right);
-
-    return 1 + Math.max(left_h, right_h);
-  }
-
   height() {
-    return this.constructor._height(this);
-  }
+    const _height = (node) => {
+      if(node == null) { return 0; }
 
-  // depth first search
-  static _dfs(node, callback){
-    if(node == null){ return; }
+      var left_h  = _height(node.left);
+      var right_h = _height(node.right);
 
-    callback(node);
-
-    this._dfs(node.left, callback);
-    this._dfs(node.right, callback);
+      return 1 + Math.max(left_h, right_h);
+    }
+    return _height(this);
   }
 
   dfs(callback){
-    this.constructor._dfs(this, callback);
+    const _dfs = (node, callback) => {
+      if(node == null){ return; }
+
+      callback(node);
+
+      _dfs(node.left, callback);
+      _dfs(node.right, callback);
+    }
+
+    _dfs(this, callback);
   }
 
   // breath first search
-  static _bfs(stack, callback){
-    if(stack.length == 0){ return; }
-
-    while(stack.length > 0){
-      var node = stack.shift();
-      callback(node);
-
-      if(node.hasLeft())  { stack.push(node.left);  }
-      if(node.hasRight()) { stack.push(node.right); }
-    }
-  }
-
-
   bfs(callback){
-    this.constructor._bfs([this], callback);
+    const _bfs = (stack, callback) => {
+      if(stack.length == 0){ return; }
+
+      while(stack.length > 0){
+        var node = stack.shift();
+        callback(node);
+
+        if(node.hasLeft())  { stack.push(node.left);  }
+        if(node.hasRight()) { stack.push(node.right); }
+      }
+    }
+
+    _bfs([this], callback);
   }
 
   // https://modernpathshala.com/Article/4141/amazon-interview-question-sde-2
