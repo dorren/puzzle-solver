5c0488cb04ffafddd7612779f0e817e1020f74a7
Dorren Chen <dorrenchen@gmail.com>
2017-09-04 23:49:31 -0400

    refactor

--- app/puzzles/poker.js
+++ app/puzzles/poker.js
@@ -16,14 +16,10 @@ var Poker  = {
     if(arr1.length != arr2.length){
         return false;
     }
-    var match = true;
-    for(let i in arr1){
-        if(arr1[i] != arr2[i]){
-            match = false;
-            break;
-        }
-    }
-    return match;
+
+    return arr1.reduce(
+             (acc, a, i) => acc && a == arr2(i),
+             true);
   },
 
   // @param val, 1 character number value only
@@ -35,15 +31,8 @@ var Poker  = {
 
   // sort hand in the order of 1,2,3...10, J, Q, K, A
   sortHand: function(hand){
-      var result = hand.sort((a, b) => {
-          let index1 = this.valueIndex(a[0]);
-          let index2 = this.valueIndex(b[0]);
-          return index1 - index2;
-      })
-      return result;
+      return hand.sort((a, b) => this.valueIndex(a[0]) - this.valueIndex(b[0]));
   },
-
-
 
   // compare same ranked/type hand, to see which one has high card.
   //
@@ -51,11 +40,11 @@ var Poker  = {
   //        == 0, equal
   //        > 1, left one is bigger
   compareHighCard: function(hand1, hand2){
-    let vals1 = hand1.map(x => {return x[0];});
-    let vals2 = hand2.map(x => {return x[0];});
+    let vals1 = hand1.map(x => x[0] );
+    let vals2 = hand2.map(x => x[0] );
     // sort by frequency values.
-    let freqKV1 = this.frequencies(vals1).sort((a, b) => {return a[1] - b[1];});
-    let freqKV2 = this.frequencies(vals2).sort((a, b) => {return a[1] - b[1];});
+    let freqKV1 = this.frequencies(vals1).sort((a, b) => a[1] - b[1] );
+    let freqKV2 = this.frequencies(vals2).sort((a, b) => a[1] - b[1] );
 
     let result = 0;
     while(result == 0 && freqKV1.length > 0){
@@ -79,19 +68,13 @@ var Poker  = {
    * @param vals, input array
    * @return frequency 2D array, [[key, freq], [key, freq], ...]
    */
-  frequencies: function(vals){
-    let hash = {};
+  frequencies: function(arr){
+    let hash = arr.reduce((h, x) => { // count element frequency
+                 h[x] = h[x] == undefined ? 1 : h[x] + 1;
+                 return h;
+               }, {});
 
-    vals.map(x => {
-        let val = x;
-        if(hash[val] == undefined){
-            hash[val] = 1;
-        }else{
-            hash[val] += 1;
-        }
-    });
-
-    let freqs = [];
+    let freqs = [];            // convert hash to 2D array.
     Object.keys(hash).sort().forEach(key => {
         freqs.push([key, hash[key]]);
     });
@@ -109,12 +92,11 @@ var Poker  = {
 
   isStraight: function(hand){
       let h = this.sortHand(hand);
-      let vals = h.map(x => {return x[0];});
+      let vals = h.map(x => this.valueIndex(x[0]) );
 
       let is_straight = true;
       for(let i=0; i< vals.length-1; i++){
-          var diff = this.valueIndex(vals[i+1]) - this.valueIndex(vals[i]);
-          if(diff != 1){
+          if(vals[i+1] - vals[i] != 1){
               is_straight = false;
               break;
           }
@@ -128,44 +110,44 @@ var Poker  = {
 
   // return type of hand, "pair", "flush", etc
   getRank: function(hand){
-      let vals = hand.map(x => {return x[0]});
-      let freqKV = this.frequencies(vals);
-      let freq = freqKV.map(x => { return x[1];}).sort();
-      let maxFreq = Math.max.apply(null, freq);
-
-      if(this.isFlush(hand)){
-        if(this.isStraight(hand)){
-            if(this.maxValue == 'A'){
-              return this.ranks.royalFlush;
-            }else{
-              return this.ranks.straightFlush;
-            }
-        }else{
-            return this.ranks.flush;
-        }
-      }
-
+    if(this.isFlush(hand)){
       if(this.isStraight(hand)){
-          return this.ranks.straight;
-      }
-
-      if(maxFreq == 1){
-          return this.ranks.highValue;
-      }else if(maxFreq == 2){
-        if(freq.length == 3){ // only 3 keys, means 2 pair
-            return this.ranks.twoPair;
-        } else {
-            return this.ranks.pair;
-        }
-      }else if(maxFreq == 3){
-          if(freq.length == 2){
-              return this.ranks.fullhouse;
+          if(this.maxValue(hand) == 'A'){
+            return this.ranks.royalFlush;
           }else{
-              return this.ranks.three;
+            return this.ranks.straightFlush;
           }
-      }else if(maxFreq == 4){
-          return this.ranks.four;
+      }else{
+          return this.ranks.flush;
       }
+    }
+
+    if(this.isStraight(hand)){
+        return this.ranks.straight;
+    }
+
+    let vals = hand.map(x => {return x[0]});
+    let freqKV = this.frequencies(vals);
+    let freq = freqKV.map(x => { return x[1];}).sort();
+    let maxFreq = Math.max.apply(null, freq);
+
+    if(maxFreq == 1){
+        return this.ranks.highValue;
+    }else if(maxFreq == 2){
+      if(freq.length == 3){ // only 3 keys, means 2 pair
+          return this.ranks.twoPair;
+      } else {
+          return this.ranks.pair;
+      }
+    }else if(maxFreq == 3){
+        if(freq.length == 2){
+            return this.ranks.fullhouse;
+        }else{
+            return this.ranks.three;
+        }
+    }else if(maxFreq == 4){
+        return this.ranks.four;
+    }
   },
 
   numToWords: function(num){
@@ -180,15 +162,14 @@ var Poker  = {
 
   // main fn
   compareHands: function(arr1, arr2){
-      var rank1 = this.getRank(arr1);
-      var rank2 = this.getRank(arr2);
+    var result = this.getRank(arr1) - this.getRank(arr2);
 
-      var result = rank1 - rank2;
-      if(result == 0){
-        result = this.compareHighCard(arr1, arr2);
-      }
-      var word = this.numToWords(result);
-      return word;
+    if(result == 0){
+      result = this.compareHighCard(arr1, arr2);
+    }
+
+    var word = this.numToWords(result);
+    return word;
   }
 }
 
